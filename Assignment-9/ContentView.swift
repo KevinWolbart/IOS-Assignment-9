22dcdc1fd58e55e746f0855e849c90220446923f
Kevin Wolbart <kevinwolbart@gmail.com>
2025-07-18 15:05:19 -0500

    Initial Commit

---
+++ Assignment-9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment-9
+//
+//  Created by Kevin Wolbart on 7/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
